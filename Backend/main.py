01a600a63c775cbcc90d1f2ed6b50679e29c9526
Juandagarc <juandagarce@gmail.com>
2023-12-01 19:50:04 -0500

    La API funciona

---
+++ Backend/main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import requests  # Make sure you also import the 'requests' module if it's not already imported
+
+app = FastAPI()
+
+# Configurar CORS para permitir todas las solicitudes desde cualquier origen
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes especificar aquí los orígenes permitidos en producción
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Reemplaza con tu token de integración de Notion
+NOTION_TOKEN = 'secret_vyOIXSQQDcpEAStRJ5hRoEkLbcqkA7EgwkkElzhcGbK'
+NOTION_DATABASE_ID = 'aca6892407d64b3e810ce12a65a5be33'
+
+NOTION_API_URL = f'https://api.notion.com/v1/databases/{NOTION_DATABASE_ID}/query'
+
+@app.get("/notion-data")
+async def get_notion_data():
+    try:
+        # Configura la conexión con la API de Notion
+        headers = {
+            'Authorization': f'Bearer {NOTION_TOKEN}',
+            'Content-Type': 'application/json',
+            'Notion-Version': '2021-05-13',  # Replace with the latest Notion API version
+        }
+
+        # Obtiene los datos de la base de datos en Notion
+        response = requests.post(NOTION_API_URL, headers=headers)
+        response.raise_for_status()
+        database_data = response.json()
+
+        # Transforma la respuesta para extraer datos específicos
+        data = database_data.get('results', [])
+
+        # Mapea los datos según la estructura de tu tabla en Notion
+        mapped_data = [
+            {
+                'nombre': item.get('properties', {}).get('Nombre', {}).get('title', [{}])[0].get('text', {}).get('content', ''),
+                'imagen': item.get('properties', {}).get('Imagen', {}).get('files', [{}])[0].get('name', ''),
+                'descripcion': item.get('properties', {}).get('Descripcion', {}).get('rich_text', [{}])[0].get('text', {}).get('content', ''),
+                'link': item.get('properties', {}).get('Link', {}).get('url', '')
+            }
+            for item in data
+        ]
+
+        # Devuelve los datos mapeados a tu aplicación de React
+        return mapped_data
+
+    except Exception as e:
+        # Maneja otros errores
+        raise HTTPException(status_code=500, detail=str(e))
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [notionData, setNotionData] = useState(null);
+
+  useEffect(() => {
+    // Llamada a la nueva ruta de tu API FastAPI que obtiene datos de Notion
+    axios.get('http://localhost:8000/notion-data')
+      .then(response => {
+        setNotionData(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <>
+      <p>¡Hola mundo!</p>
+      {notionData && (
+        <div>
+          {/* Mapea sobre los datos de Notion y muestra la información */}
+          {notionData.map(item => (
+            <div key={item.imagen}>
+              <h2>{item.nombre}</h2>
+              <img src={item.imagen} alt={item.nombre} />
+              <p>{item.descripcion}</p>
+              {item.link && (
+                <a href={item.link} target="_blank" rel="noopener noreferrer">
+                  Enlace
+                </a>
+              )}
+              <hr />
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
